b1d23f60afa1f4288c3e1c903e677f4153c8df2e
Mahmoud Hamam <mahmoudhamam093@gmail.com>
2026-07-14 02:05:09 +0300

    Refactor

--- lib/features/home/presentation/widgets/all_product_gridview.dart
+++ lib/features/home/presentation/widgets/all_product_gridview.dart
@@ -20,29 +20,20 @@ class AllProductGridView extends StatelessWidget {
         } else if (state is ProductsSuccessState) {
           return GridView.builder(
             shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
+            physics: NeverScrollableScrollPhysics(),
 
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        childAspectRatio: 0.70,
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-
-          elevation: 0,
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ProductDetailsScreen(productId: '123e4567-e89b-12d3-a456-426614174000'),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 0.70,
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: state.products.length,
+            itemBuilder: (context, index) {
+              final product = state.products[index];
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
                 ),
 
                 elevation: 0,
@@ -52,7 +43,8 @@ class AllProductGridView extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const ProductDetailsScreen(),
+                        builder: (context) =>
+                            ProductDetailsScreen(productId: product.id),
                       ),
                     );
                   },
@@ -70,7 +62,7 @@ class AllProductGridView extends StatelessWidget {
                                 width: double.infinity,
                                 height: double.infinity,
                                 errorBuilder: (context, error, stackTrace) {
-                                  return const Icon(Icons.image_not_supported);
+                                  return Icon(Icons.image_not_supported);
                                 },
                               ),
                             ),
@@ -87,7 +79,7 @@ class AllProductGridView extends StatelessWidget {
                                 ),
                                 child: IconButton(
                                   onPressed: () {},
-                                  icon: const Icon(
+                                  icon: Icon(
                                     Icons.favorite_border,
                                     color: AppColors.darkBackground,
                                     size: 18,
@@ -116,7 +108,7 @@ class AllProductGridView extends StatelessWidget {
                             "${product.price} SAR",
                             style: TextStyle(
                               fontWeight: FontWeight.w400,
-                              color: const Color.fromARGB(255, 158, 125, 16),
+                              color: Color.fromARGB(255, 158, 125, 16),
                               fontSize: 16,
                             ),
                           ),
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'package:nti_ecommerce_team4/core/theme/app_theme.dart';
 import 'package:nti_ecommerce_team4/features/auth/presentation/screens/login_screen.dart';
 import 'package:provider/provider.dart';
 
-import 'core/network/dio_helper.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
